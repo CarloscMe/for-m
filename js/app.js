1e8acded51b4984f9283a0a76c3c4f3037613d95
carloscMe <carloscarrillo.sprout@gmail.com>
2025-02-08 17:48:58 -0500

    mejoras-2

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", ()=> {
+     
+    confirm("¿estas seguro de enviar los datos?");
+    alert("gracias por enviarlos");
+});
