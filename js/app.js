58c0ebcca55c9fc24b38451500b6f7ce0824d492
carloscMe <carloscarrillo.sprout@gmail.com>
2025-02-10 15:43:42 -0500

    responsive

--- js/app.js
+++ js/app.js
@@ -4,4 +4,7 @@ submit.addEventListener("click", ()=> {
      
     confirm("¿estas seguro de enviar los datos?");
     alert("gracias por enviarlos");
+    form = document.querySelector("form").style.boxShadow = "0 0 20px white";;
+
 });
+
