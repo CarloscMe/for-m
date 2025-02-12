e5c7de0c633a8a0e39923dd510e9a488c8da901e
carloscMe <carloscarrillo.sprout@gmail.com>
2025-02-12 14:44:23 -0500

    algo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const submit = document.getElementById("submit");
 
+const name = document.getElementById("name");
+
 submit.addEventListener("click", ()=> {
      
     confirm("¿estas seguro de enviar los datos?");
     alert("gracias por enviarlos");
-    form = document.querySelector("form").style.boxShadow = "0 0 20px white";;
+    form = document.querySelector("form").style.boxShadow = "0 0 20px #fff";;
 
 });
 
