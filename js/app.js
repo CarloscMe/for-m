3099510384c57a26c04ab767e0dbab90c8bcee39
carloscMe <carloscarrillo.sprout@gmail.com>
2025-05-26 16:42:39 -0500

    counter

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 const submit = document.getElementById("submit");
-const form = document.querySelector("form")
+const form = document.querySelector("form");
 const name = document.getElementById("#name");
+
+const reseña = document.querySelector("#reseña");
+const contador = document.querySelector(".contador");
+const aumentar = document.querySelector(".aumentar");
+const limite = document.querySelector(".limite");
 
 submit.addEventListener("submit", (event)=> {
     event.preventDefault();
@@ -11,3 +16,15 @@ submit.addEventListener("submit", (event)=> {
 
 });
 
+reseña.addEventListener("input", (e) => {
+  
+  aumentar.textContent = e.target.value.length;
+
+  if(e.target.value.length === 250){
+    limite.style.color = "#fa0606";
+  } else{
+    aumentar.style.color = "#2bff01";
+    limite.style.color = "#fff";
+  };
+
+});
